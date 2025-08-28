dba56ec44592f9636b2cc3fd663caf878769f77d
Yohei Ohashi <hashi.life@outlook.jp>
2025-08-29 01:01:14 +0900

    Lesson36: リスト2

--- basic/base9.py
+++ basic/base9.py
@@ -1,13 +1,29 @@
 # リスト
-list_a = [1, 2, 3, 4]
-list_b = []
+# list_a = [1, 2, 3, 4]
+# list_b = []
 
-print(list_a)
-print(list_a[-2])
+# print(list_a)
+# print(list_a[-2])
 
-list_a = [1, [1, 2, 'apple'], 3, 'banana']
+# list_a = [1, [1, 2, 'apple'], 3, 'banana']
 
-print(list_a[1][2])
+# print(list_a[1][2])
+# list_a[1][2] = 'lemon'
+
+# リストのメソッド
+
+list_a = [1, 2, 3, 4, 5]
+
+list_b = list_a[1:4:2]
+print(list_b)
+
+# append, extend, insert, clear
+
+list_a.append('apple')
 print(list_a)
-list_a[1][2] = 'lemon'
+list_a.extend(['banana', 'melon'])
 print(list_a)
+list_a.insert(1, 'grape')
+print(list_a)
+# list_a.clear()
+# print(list_a)

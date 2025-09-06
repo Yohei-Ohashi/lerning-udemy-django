a8449accaf40b82301191054561aa4b207f97f7c
Yohei Ohashi <hashi.life@outlook.jp>
2025-09-06 19:27:16 +0900

    Lesson59: 関数1

---
+++ 02_function/base1.py
@@ -0,0 +1,38 @@
+# 関数
+
+
+def print_hello():
+    print("Hello World")
+
+
+print(type(print_hello))
+print(id(print_hello))
+print_hello()
+
+abc = print_hello
+print(id(abc))
+abc()
+
+
+def max_num(a, b):
+    print(f"a = {a}, b = {b}")
+    if a > b:
+        return a
+    else:
+        return b
+
+
+c = max_num(10, 200)
+d = max_num(30, 20)
+print(c)
+print(f"d: {d}")
+
+e = max_num(b=19, a=21)
+print(e)
+
+num = {
+    "a": 20,
+    "b": 30
+}
+
+print(max_num(**num))
